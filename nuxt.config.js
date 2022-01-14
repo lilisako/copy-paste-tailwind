02518b9d60a735b3690198578e82aa9d6816ac89
risako <lilisako062397@gmail.com>
2022-01-14 15:41:05 +0900

    fix seo

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,15 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { hid: 'description', name: 'description', content: 'Just copy & paste Tailwind code!' },
+      { name: 'format-detection', content: 'telephone=no' },
+      { hid: 'og:site_name', property: 'og:site_name', content: 'Copy-Paste Tailwind' },
+      { hid: 'og:type', property: 'og:type', content: 'website' },
+      { hid: 'og:url', property: 'og:url', content: 'https://tailwind.risako.dev' },
+      { hid: 'og:title', property: 'og:title', content: 'Copy-Paste Tailwind' },
+      { hid: 'og:description', property: 'og:description', content: 'Just copy & paste Tailwind code!' },
+      { hid: 'og:image', property: 'og:image', content: 'https://tailwind.risako.dev/ogp.png' },
+      { name: 'twitter:card', content: 'summary_large_image' },
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
