fd3ea9066df09f28e340e9547204b8cacddb5a5d
risako <lilisako062397@gmail.com>
2022-01-14 13:53:02 +0900

    fix: eslint

--- components/news-article-card.vue
+++ components/news-article-card.vue
@@ -1,26 +1,38 @@
 <template>
   <div class="p-6">
     <div class="flex items-center py-2">
-      <img src="/rome.png" alt="Rome" class="w-16 h-16 rounded-md mr-2">
+      <img src="/rome.png" alt="Rome" class="mr-2 w-16 h-16 rounded-md">
       <div class="px-2">
-        <h2 class="font-bold text-base">7 best places you should visit in Rome</h2>
-        <div class="text-xs text-gray-400 flex">
+        <h2 class="text-base font-bold">
+          7 best places you should visit in Rome
+        </h2>
+        <div class="flex text-xs text-gray-400">
           <span class="text-xs material-icons">schedule</span>
-          <p class="mx-1">6 hours ago</p>
+          <p class="mx-1">
+            6 hours ago
+          </p>
           <span class="text-xs material-icons">visibility</span>
-          <p class="mx-1">2301 views</p>
+          <p class="mx-1">
+            2301 views
+          </p>
         </div>
       </div>
     </div>
     <div class="flex items-center py-2">
-      <img src="/rome2.png" alt="Rome" class="w-16 h-16 rounded-md mr-2">
+      <img src="/rome2.png" alt="Rome" class="mr-2 w-16 h-16 rounded-md">
       <div class="px-2">
-        <h2 class="font-bold text-base">10 Unusual Things to Do in Rome to Escape the Crowds</h2>
-        <div class="text-xs text-gray-400 flex">
+        <h2 class="text-base font-bold">
+          10 Unusual Things to Do in Rome to Escape the Crowds
+        </h2>
+        <div class="flex text-xs text-gray-400">
           <span class="text-xs material-icons">schedule</span>
-          <p class="mx-1">2 days ago</p>
+          <p class="mx-1">
+            2 days ago
+          </p>
           <span class="text-xs material-icons">visibility</span>
-          <p class="mx-1">15000 views</p>
+          <p class="mx-1">
+            15000 views
+          </p>
         </div>
       </div>
     </div>
