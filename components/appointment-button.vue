004895f0a8e9bfa50fb6d5e934d713f56994100f
risako <lilisako062397@gmail.com>
2022-01-14 14:09:11 +0900

    fix: delete ts

--- components/appointment-button.vue
+++ components/appointment-button.vue
@@ -37,9 +37,3 @@
     </div>
   </div>
 </template>
-
-<script lang="ts">
-import Vue from 'vue'
-
-export default Vue.extend({})
-</script>
--- components/news-article-card.vue
+++ components/news-article-card.vue
@@ -38,9 +38,3 @@
     </div>
   </div>
 </template>
-
-<script lang="ts">
-import Vue from 'vue'
-
-export default Vue.extend({})
-</script>
