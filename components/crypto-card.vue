db82de8d1e76c932ebbbf0c60a9a39f10ab16d64
risako <lilisako062397@gmail.com>
2022-01-14 14:38:02 +0900

    fix: eslint

--- components/crypto-card.vue
+++ components/crypto-card.vue
@@ -1,15 +1,23 @@
 <template>
-  <div class="bg-gray-50 shadow-md p-4 rounded-xl">
+  <div class="p-4 bg-gray-50 rounded-xl shadow-md">
     <div class="flex items-center py-4">
-      <img src="https://bitcoin.org/img/icons/opengraph.png?1641218872" class="rounded-full w-12 h-12">
+      <img src="https://bitcoin.org/img/icons/opengraph.png?1641218872" class="w-12 h-12 rounded-full">
       <div class="mx-4">
-        <p class="font-bold">BTC</p>
-        <p class="text-gray-400">Bitcoin</p>
+        <p class="font-bold">
+          BTC
+        </p>
+        <p class="text-gray-400">
+          Bitcoin
+        </p>
       </div>
     </div>
     <div class="flex items-center">
-      <p class="font-bold mr-8">$24,902</p>
-      <p class="text-green-400">↗︎2.41%</p>
+      <p class="mr-8 font-bold">
+        $24,902
+      </p>
+      <p class="text-green-400">
+        ↗︎2.41%
+      </p>
     </div>
   </div>
 </template>
